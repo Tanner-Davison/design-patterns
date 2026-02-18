b356e4672c7741ee3a70368284992e90f2ae7336
Tanner Davison <tanner.davison95@gmail.com>
2026-02-18 14:10:22 -0700

    saving docs

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <variant>
+
+using ReturnType = std::variant<int, bool>;
+struct IOperation {
+    virtual ReturnType ResultOf(int* beg, int* end) = 0;
+    virtual ~IOperation()                           = default;
+};
+
+class Operation : public IOperation {
+  public:
+    virtual ReturnType ResultOf(int* beg, int* end) {
+
+    };
+};
+
+class BoolOperation : public IOperation {
+  public:
+    virtual ReturnType ResultOf(int* beg, int* end) override {}
+};
+
+void Operate(IOperation* op) {
+    using namespace std;
+    int  arr[5]{};
+    auto result = op->ResultOf(arr, arr + 5);
+}
 
 int main() {
     std::cout << "Program running " << std::endl;
